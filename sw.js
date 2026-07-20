4cefc3fa4b319fb9d3930d333fc513e692c482c1
tad551216-rgb <tad551216@gmail.com>
2026-07-20 17:18:59 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,20 @@
+const CACHE = 'sougei-keikaku-v1';
+const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png', './icon-180.png'];
+self.addEventListener('install', e=>{
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()));
+});
+self.addEventListener('activate', e=>{
+  e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));
+});
+self.addEventListener('fetch', e=>{
+  if(e.request.method !== 'GET') return;
+  const url = new URL(e.request.url);
+  if(url.origin !== location.origin){ return; } // 地理院・OSM などはそのままネットへ
+  e.respondWith(
+    fetch(e.request).then(res=>{
+      const cp = res.clone();
+      caches.open(CACHE).then(c=>c.put(e.request, cp));
+      return res;
+    }).catch(()=>caches.match(e.request).then(r=>r || caches.match('./index.html')))
+  );
+});
